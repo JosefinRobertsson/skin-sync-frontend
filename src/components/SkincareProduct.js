ecd8afc52c71014e1dbfa5845c7bbe6567e73552
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-06-01 17:05:12 +0200

    delete funtion for the porducts and visibility of the products

--- src/components/SkincareProduct.js
+++ src/components/SkincareProduct.js
@@ -1,10 +1,29 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-console */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function SkincareProduct() {
   const [name, setName] = useState('');
   const [brand, setBrand] = useState('');
+  const [products, setProducts] = useState([]);
+
+  const getProducts = () => {
+    const accessToken = localStorage.getItem('accessToken');
+    fetch('http://localhost:8080/skincareProduct', {
+      headers: {
+        Authorization: accessToken,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.response);
+      });
+  };
+
+  useEffect(() => {
+    getProducts(); // display the products
+  }, []);
 
   const handleSubmitProduct = (event) => {
     event.preventDefault();
@@ -28,12 +47,32 @@ function SkincareProduct() {
           console.log('Skincare product submitted successfully');
           setName('');
           setBrand('');
+          getProducts(); // Refresh products list after adding a new product
         } else {
           console.error('Failed to submit Skincare Product');
         }
       })
       .catch((error) => {
         console.error('An error occurred:', error);
+      });
+  };
+
+  const handleDeleteProduct = (productId) => {
+    const accessToken = localStorage.getItem('accessToken');
+    fetch(`http://localhost:8080/skincareProduct/${productId}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: accessToken,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          console.log('Product deleted successfully');
+          getProducts(); // Refresh products list after deleting a product
+        } else {
+          console.error('Failed to delete product');
+        }
       });
   };
 
@@ -61,6 +100,21 @@ function SkincareProduct() {
         </div>
         <button type="submit">Submit Product</button>
       </form>
+      <h2>Added Products</h2>
+      {products.map((product) => (
+        <div key={product._id}>
+          <p>
+            Product Type:
+            {product.name}
+
+            brand name:
+            {' '}
+            {product.brand}
+
+          </p>
+          <button type="button" onClick={() => handleDeleteProduct(product._id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 }
