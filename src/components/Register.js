7882692cfb8bf74344d3d1298babf905ac96e92b
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-06-01 12:31:42 +0200

    daily report working

--- src/components/Register.js
+++ src/components/Register.js
@@ -17,18 +17,8 @@ function Register() {
       })
       .then((response) => {
         console.log(response);
-        // Assuming the server sends back the access token after a successful registration
         const token = response.data.accessToken;
         localStorage.setItem('accessToken', token);
-
-        // Now we can make a call to the secure endpoint
-        axios.get('http://localhost:8080/secure-endpoint', {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-          .then((response) => console.log(response))
-          .catch((error) => console.error(error));
       })
       .catch((error) => console.error(error));
   };
