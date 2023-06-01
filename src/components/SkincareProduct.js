8386b87986ca7031ddeaaaa9c60b43cbf4788d6d
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-06-01 20:53:03 +0200

    products divided in morning and nigth

--- src/components/SkincareProduct.js
+++ src/components/SkincareProduct.js
@@ -1,31 +1,96 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-underscore-dangle */
-/* eslint-disable no-console */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState, useEffect } from 'react';
 
 function SkincareProduct() {
-  const [name, setName] = useState('');
-  const [brand, setBrand] = useState('');
-  const [products, setProducts] = useState([]);
+  const [morningName, setMorningName] = useState('');
+  const [morningBrand, setMorningBrand] = useState('');
+  const [nightName, setNightName] = useState('');
+  const [nightBrand, setNightBrand] = useState('');
+  const [morningProducts, setMorningProducts] = useState([]);
+  const [nightProducts, setNightProducts] = useState([]);
 
-  const getProducts = () => {
+  const getMorningProducts = () => {
+    // Replace with your actual API call for morning products
     const accessToken = localStorage.getItem('accessToken');
-    fetch('http://localhost:8080/skincareProduct', {
+    fetch('http://localhost:8080/skincareProduct/morning', {
+      headers: {
+        Authorization: accessToken,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Error: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setMorningProducts(data.response);
+      })
+      .catch((error) => {
+        console.error('An error occurred:', error);
+      });
+  };
+
+  const getNightProducts = () => {
+    // Replace with your actual API call for night products
+    const accessToken = localStorage.getItem('accessToken');
+    fetch('http://localhost:8080/skincareProduct/night', {
       headers: {
         Authorization: accessToken,
       },
     })
       .then((res) => res.json())
       .then((data) => {
-        setProducts(data.response);
+        setNightProducts(data.response);
       });
   };
 
   useEffect(() => {
-    getProducts(); // display the products
+    getMorningProducts();
+    getNightProducts();
   }, []);
 
-  const handleSubmitProduct = (event) => {
+  // MORNING
+
+  const handleSubmitMorningRoutine = (event) => {
+    event.preventDefault();
+    const accessToken = localStorage.getItem('accessToken');
+    // eslint-disable-next-line no-unused-vars
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: accessToken,
+      },
+      body: JSON.stringify({
+        name: morningName,
+        brand: morningBrand,
+        routine: 'night',
+      }),
+    };
+
+    fetch('http://localhost:8080/skincareProduct', options)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          console.log('Morning routine product submitted successfully');
+          setMorningName('');
+          setMorningBrand('');
+          getMorningProducts();
+        } else {
+          console.error('Failed to submit Skincare Product');
+        }
+      })
+      .catch((error) => {
+        console.error('An error occurred:', error);
+      });
+  };
+
+  // NIGTH
+
+  const handleSubmitNightRoutine = (event) => {
     event.preventDefault();
     const accessToken = localStorage.getItem('accessToken');
     const options = {
@@ -35,8 +100,9 @@ function SkincareProduct() {
         Authorization: accessToken,
       },
       body: JSON.stringify({
-        name,
-        brand,
+        name: nightName,
+        brand: nightBrand,
+        routine: 'night',
       }),
     };
 
@@ -44,10 +110,10 @@ function SkincareProduct() {
       .then((response) => response.json())
       .then((data) => {
         if (data.success) {
-          console.log('Skincare product submitted successfully');
-          setName('');
-          setBrand('');
-          getProducts(); // Refresh products list after adding a new product
+          console.log('Night routine product submitted successfully');
+          setNightName('');
+          setNightBrand('');
+          getNightProducts();
         } else {
           console.error('Failed to submit Skincare Product');
         }
@@ -56,6 +122,8 @@ function SkincareProduct() {
         console.error('An error occurred:', error);
       });
   };
+
+  // DELETE
 
   const handleDeleteProduct = (productId) => {
     const accessToken = localStorage.getItem('accessToken');
@@ -69,7 +137,8 @@ function SkincareProduct() {
       .then((data) => {
         if (data.success) {
           console.log('Product deleted successfully');
-          getProducts(); // Refresh products list after deleting a product
+          getMorningProducts();
+          getNightProducts();
         } else {
           console.error('Failed to delete product');
         }
@@ -78,39 +147,62 @@ function SkincareProduct() {
 
   return (
     <div>
-      <h1>Products shelf</h1>
-      <form onSubmit={handleSubmitProduct}>
+      <h1>Products Shelf</h1>
+      <form onSubmit={handleSubmitMorningRoutine}>
+        <h1>Upload your Morning routine</h1>
         <div>
-          <label htmlFor="name">Name of the product:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+          <label htmlFor="morningName">Name of the product:</label>
+          <input type="text" id="morningName" value={morningName} onChange={(e) => setMorningName(e.target.value)} />
         </div>
         <div>
-          <label htmlFor="brand">Brand of the product:</label>
-          <input
-            type="text"
-            id="brand"
-            value={brand}
-            onChange={(e) => setBrand(e.target.value)}
-          />
+          <label htmlFor="morningBrand">Brand of the product:</label>
+          <input type="text" id="morningBrand" value={morningBrand} onChange={(e) => setMorningBrand(e.target.value)} />
         </div>
         <button type="submit">Submit Product</button>
       </form>
-      <h2>Added Products</h2>
-      {products.map((product) => (
+
+      <form onSubmit={handleSubmitNightRoutine}>
+        <h1>Upload your Night Routine</h1>
+        <div>
+          <label htmlFor="nightName">Name of the product:</label>
+          <input type="text" id="nightName" value={nightName} onChange={(e) => setNightName(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="nightBrand">Brand of the product:</label>
+          <input type="text" id="nightBrand" value={nightBrand} onChange={(e) => setNightBrand(e.target.value)} />
+        </div>
+        <button type="submit">Submit Product</button>
+      </form>
+
+      <h2>Added Products for the Morning routine</h2>
+      {morningProducts.map((product) => (
         <div key={product._id}>
           <p>
-            Product Type:
+            Product Name:
+            {' '}
             {product.name}
-
-            brand name:
+            {' '}
+            <br />
+            Brand:
             {' '}
             {product.brand}
+          </p>
+          <button type="button" onClick={() => handleDeleteProduct(product._id)}>Delete</button>
+        </div>
+      ))}
 
+      <h2>Added Products for the Night routine</h2>
+      {nightProducts.map((product) => (
+        <div key={product._id}>
+          <p>
+            Product Name:
+            {' '}
+            {product.name}
+            {' '}
+            <br />
+            Brand:
+            {' '}
+            {product.brand}
           </p>
           <button type="button" onClick={() => handleDeleteProduct(product._id)}>Delete</button>
         </div>
