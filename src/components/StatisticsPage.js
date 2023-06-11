933fda9993cb3b7fd6e9f91546507c9d0de47600
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-06-11 22:27:27 +0200

    function created in front to filter data for displaying in weeks and function to create an average of that data

--- src/components/StatisticsPage.js
+++ src/components/StatisticsPage.js
@@ -1,6 +1,68 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+// Extend Date object to get the week number
+
+// eslint-disable-next-line no-extend-native, func-names
+Date.prototype.getWeek = function () {
+  const date = new Date(this);
+  date.setHours(0, 0, 0, 0);
+  date.setDate(date.getDate() + 4 - (date.getDay() || 7));
+
+  const yearStart = new Date(date.getFullYear(), 0, 1);
+  const weekNumber = Math.ceil((((date - yearStart) / 86400000) + 1) / 7);
+
+  return weekNumber;
+};
+
+const FilterData = (weekNumber, data) => {
+  const filteredData = [];
+
+  for (let i = 0; i < data.length; i++) {
+    const elementDate = new Date(data[i].date);
+    const elementWeek = elementDate.getWeek();
+
+    if (elementWeek === weekNumber) {
+      filteredData.push(data[i])
+    }
+  }
+
+  return filteredData
+}
+
+const AverageData = (data) => {
+  const average = {
+    acne: 0,
+    alcohol: 0,
+    dairy: 0,
+    exercised: 0,
+    period: 0,
+    sleepHours: 0,
+    stress: 0,
+    sugar: 0,
+    waterAmount: 0
+  };
+
+  const lengthData = data.length;
+
+  for (let i = 0; i < lengthData; i++) {
+    const element = data[i];
+    average.acne += element.acne / lengthData
+    average.alcohol += element.alcohol / lengthData
+    average.dairy += element.dairy / lengthData
+    average.exercised += element.exercised / lengthData
+    average.period += element.period / lengthData
+    average.sleepHours += element.sleepHours / lengthData
+    average.stress += element.stress / lengthData
+    average.sugar += element.sugar / lengthData
+    average.waterAmount += element.waterAmount / lengthData
+  }
+
+  return average
+}
 
 const StatisticsPage = () => {
   const [data, setData] = useState({});
@@ -34,13 +96,17 @@ const StatisticsPage = () => {
   }, []);
 
   console.log('data for barchart:', data);
+  const filteredData = FilterData(25, data)
+  console.log('filtered data', filteredData)
+  const averageData = AverageData(filteredData)
+  console.log('averaged data', averageData)
 
   return (
     <div>
 
       <h1>Chart 1</h1>
 
-      <ResponsiveContainer width="50%" height={400}>
+      <ResponsiveContainer width="100%" height={400}>
         <BarChart data={data} margin={{ top: 10, left: 20, right: 20, bottom: 10 }}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="date" />
@@ -57,7 +123,7 @@ const StatisticsPage = () => {
 
       <h1>Chart 2</h1>
 
-      <ResponsiveContainer width="50%" height={400}>
+      <ResponsiveContainer width="100%" height={400}>
         <BarChart data={data} margin={{ top: 10, left: 20, right: 20, bottom: 10 }}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="date" />
