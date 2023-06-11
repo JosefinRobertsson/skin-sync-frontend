dea98f8bf946ed78baba90b4dbc7a6029edb04c1
JosefinRobertsson <josefinrobertsson@hotmail.com>
2023-06-11 19:46:16 +0200

    checks usedToday status and date when page loads

--- src/components/UsageTracker.js
+++ src/components/UsageTracker.js
@@ -26,10 +26,11 @@ const settings = {
   swipe: true,
   infinite: true,
   speed: 500,
-  slidesToShow: 2,
+  slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: false,
-  autoplaySpeed: 2000
+  autoplaySpeed: 2000,
+  centerMode: true
 };
 
 const getImagePath = (category) => {
@@ -48,28 +49,7 @@ const getImagePath = (category) => {
       return defaultImage;
   }
 };
-/*
-const SingleItem = ({ product, handleUsageChange }) => (
-  <>
-    <div className="productItem" key={product._id}>
-      <img
-        src={getImagePath(product.category)}
-        alt={product.category} />
-      <p>Name: {product.name}</p>
-      <p>Brand: {product.brand}</p>
-    </div>
-    <div>
-      <label htmlFor={`toggle-${product._id}`}>
-        Used today:
-        <Toggle
-          id={`toggle-${product._id}`}
-          checked={product.usedToday}
-          onChange={() => handleUsageChange(product)} />
-      </label>
-    </div>
-  </>
-);
-*/
+
 const UsageTracker = () => {
   const [morningProducts, setMorningProducts] = useState([]);
   const [nightProducts, setNightProducts] = useState([]);
@@ -91,13 +71,52 @@ const UsageTracker = () => {
           }
         };
 
+        // Fetch all products
         const morningResponse = await axios.get('/productShelf/morning', config);
         const nightResponse = await axios.get('/productShelf/night', config);
         if (!morningResponse.data.success || !nightResponse.data.success) {
           throw new Error('Failed to fetch skincare products');
         }
-        setMorningProducts(morningResponse.data.response);
-        setNightProducts(nightResponse.data.response);
+
+        const morningProducts = morningResponse.data.response;
+        const nightProducts = nightResponse.data.response;
+
+        // Check and reset usedToday status if new day
+        const now = new Date();
+        const todayDate = now.toISOString().split('T')[0];
+
+        morningProducts.forEach(async (product) => {
+          if (product.usedToday) {
+            // compare last usage date with today's date
+            const lastUsageDate = product.usageHistory[product.usageHistory.length - 1];
+            const lastUsageDateStr = lastUsageDate && lastUsageDate instanceof Date ? lastUsageDate.toISOString().split('T')[0] : null;
+
+            if (lastUsageDateStr !== todayDate) {
+              // Send request to reset usedToday
+              await axios.post('/productShelf/logUsage', {
+                productId: product._id,
+                usedToday: false
+              }, config);
+            }
+          }
+        });
+
+        nightProducts.forEach(async (product) => {
+          if (product.usedToday) {
+            const lastUsageDate = product.usageHistory[product.usageHistory.length - 1];
+            const lastUsageDateStr = lastUsageDate && lastUsageDate instanceof Date ? lastUsageDate.toISOString().split('T')[0] : null;
+
+            if (lastUsageDateStr !== todayDate) {
+              await axios.post('/productShelf/logUsage', {
+                productId: product._id,
+                usedToday: false
+              }, config);
+            }
+          }
+        });
+
+        setMorningProducts(morningProducts);
+        setNightProducts(nightProducts);
         setIsLoading(false);
       } catch (e) {
         console.error(e);
@@ -108,9 +127,10 @@ const UsageTracker = () => {
     fetchSkincareProducts();
   }, []);
 
-  const handleUsageChange = async (product) => {
+  /*
+  // Change usage status of all products in a routine
+  const toggleAllUsage = async (action) => {
     try {
-      const { _id: productId, usedToday, usageHistory } = product;
       const accessToken = localStorage.getItem('accessToken');
       if (!accessToken) {
         setError('No access token found');
@@ -121,6 +141,70 @@ const UsageTracker = () => {
           Authorization: accessToken
         }
       };
+
+      const toggleProducts = async (products) => {
+        const updatedProducts = [];
+
+        for (let i = 0; i < products.length; i++) {
+          const product = products[i];
+          let updatedProduct;
+
+          if (action === 'toggleOn' && !product.usedToday) {
+            // Set usedToday to true and push the current date to usageHistory
+            updatedProduct = {
+              ...product,
+              usedToday: true,
+              usageHistory: [...product.usageHistory, new Date()]
+            };
+          } else if (action === 'toggleOff' && product.usedToday) {
+            // Set usedToday to false and pop the last date from usageHistory
+            const updatedUsageHistory = [...product.usageHistory];
+            updatedUsageHistory.pop();
+
+            updatedProduct = {
+              ...product,
+              usedToday: false,
+              usageHistory: updatedUsageHistory
+            };
+          } else {
+            updatedProduct = product; // No changes needed for this product
+          }
+
+          // Send request to update the product
+          const response = await axios.post('/productShelf/toggleAllUsage', updatedProduct, config);
+          updatedProducts.push(response.data.response);
+        }
+
+        return updatedProducts;
+      };
+
+      const updatedMorningProducts = await toggleProducts(morningProducts);
+      const updatedNightProducts = await toggleProducts(nightProducts);
+
+      setMorningProducts(updatedMorningProducts);
+      setNightProducts(updatedNightProducts);
+    } catch (error) {
+      console.error(error);
+      setError('Failed to toggle usage for all products');
+    }
+  };
+ */
+
+  // Change usage status of a single product
+  const handleUsageChange = async (product) => {
+    try {
+      const { _id: productId, usedToday } = product;
+      const accessToken = localStorage.getItem('accessToken');
+      if (!accessToken) {
+        setError('No access token found');
+        return;
+      }
+      const config = {
+        headers: {
+          Authorization: accessToken
+        }
+      };
+      /*
       const updatedUsedToday = !usedToday;
       const updatedUsageHistory = [...usageHistory];
       if (updatedUsedToday) {
@@ -130,13 +214,12 @@ const UsageTracker = () => {
         console.log('popped:', updatedUsageHistory);
         console.log('length:', updatedUsageHistory.length);
       }
-
+      */
       const response = await axios.post(
         '/productShelf/logUsage',
         {
           productId,
-          usedToday: updatedUsedToday,
-          usageHistory: updatedUsageHistory
+          usedToday: !usedToday
         },
         config
       );
@@ -145,18 +228,19 @@ const UsageTracker = () => {
         console.log('response:', response.data.response);
         setMorningProducts(
           (prevMorningProducts) => prevMorningProducts.map((product) => (product._id === productId
-            ? { ...product, usedToday: updatedUsedToday, usageHistory: updatedUsageHistory }
+            ? { ...product, usedToday: !usedToday }
             : product))
         );
 
         setNightProducts(
           (prevNightProducts) => prevNightProducts.map((product) => (product._id === productId
-            ? { ...product, usedToday: updatedUsedToday, usageHistory: updatedUsageHistory }
+            ? { ...product, usedToday: !usedToday }
             : product))
         );
       }
     } catch (e) {
       console.error(e);
+      console.error('product category:', e.response?.data?.response?.category || 'Unknown');
     }
   };
 
@@ -191,6 +275,7 @@ const UsageTracker = () => {
           </div>
         ))}
       </Slider>
+
       <h2>Night Routine</h2>
       <div>
         <ShelfLink to="/productShelf">Edit routine</ShelfLink>
@@ -218,3 +303,9 @@ const UsageTracker = () => {
 };
 
 export default UsageTracker;
+
+/* <Toggle
+        id="toggle-all-night"
+        checked={morningProducts.every((product) => product.usedToday)}
+        onChange={() => toggleAllUsage()} />
+        */
