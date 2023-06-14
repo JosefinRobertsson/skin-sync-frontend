d919f093af771e1bf067bbe01fc5dc8c4477b236
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-06-14 16:23:54 +0200

    user page added elements

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { DailyLink, ShelfLink, StatisticsLink } from '../styles/StyledLinks';
 import './UserPage.css'
+import dailyImage from '../images/dailyImage.png';
+import shelfImage from '../images/shelfImage.png';
+import statisticsImage from '../images/statisticsImage.png';
 
 const UserPage = () => {
   const [uvIndex, setUvIndex] = useState(null);
@@ -22,27 +25,46 @@ const UserPage = () => {
   }, []);
 
   return (
-    <>
+    <div className="user-page">
       <div>
-        <h1>Welcome Back!</h1>
-        {uvIndex !== null && (
-          <p>
-          The UV index is:
-            {' '}
-            {uvIndex}
-            <h2>Rember to wear sunscreen!</h2>
-          </p>
+        <div>
+          <h1>Welcome Back!</h1>
+          {uvIndex !== null && (
+            <p>
+              The UV index is: {uvIndex}
+              <h2>Remember to wear sunscreen!</h2>
+            </p>
+          )}
+        </div>
 
-        )}
+        <div className="imagesandbuttons">
+          <div>
+
+            <img src={dailyImage} alt="Daily Report" className="button-image" />
+            <DailyLink to="/DailyReport" className="route-button">
+            Log day
+            </DailyLink>
+          </div>
+
+          <div>
+            <img src={shelfImage} alt="Product Shelf" className="button-image" />
+            <ShelfLink to="/productShelf" className="route-button">
+            Product shelf
+            </ShelfLink>
+          </div>
+
+          <div>
+            <img src={statisticsImage} alt="Statistics" className="button-image" />
+            <StatisticsLink to="/statisticsPage" className="route-button">
+            Statistics
+            </StatisticsLink>
+          </div>
+
+        </div>
       </div>
-      <h2>How was your day?</h2>
-      <DailyLink to="/DailyReport" className="route-button">Log day</DailyLink>
-      <h2> Log your skin care here</h2>
-      <ShelfLink to="/productShelf" className="route-button">Product shelf</ShelfLink>
-      <h2> See your progress </h2>
-      <StatisticsLink to="/statisticsPage" className="route-button">Statistics</StatisticsLink>
-    </>
+
+    </div>
   );
-}
+};
 
 export default UserPage;
