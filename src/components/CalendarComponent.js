39f1984024b2e724922f86ec5b1b805091e91cdb
JosefinRobertsson <josefinrobertsson@hotmail.com>
2023-06-15 22:56:09 +0200

    styled div put back

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -1,6 +1,24 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  background-color:#1f101f;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+const StyledH1 = styled.h1`
+  padding-top: 20%;
+  padding-bottom: 0%;
+  font-size: 32px;
+  letter-spacing: 0.5%;
+  text-align: center;
+  color: #FFF4E9;
+`;
 
 const CalendarComponent = ({ onDateChoice }) => {
   const [chosenDate, setChosenDate] = useState(new Date());
